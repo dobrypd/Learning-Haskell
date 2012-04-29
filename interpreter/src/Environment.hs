ec0616facd0d9b5db4565da0d1b02e85aba0fd43
Piotr Dobrowolski <dobrypd@gmail.com>
2012-04-29 14:09:12 +0200

    first working expressions

--- interpreter/src/Environment.hs
+++ interpreter/src/Environment.hs
@@ -13,7 +13,7 @@ import AbsKappaGrammar
 import PrintKappaGrammar
 import ErrM
 
-data Value = VInt Integer | VFloat Double | VBoolean Bool | VStruct [(Ident, Value)] | Undefined | ErrorOccurred
+data Value = VInt Integer | VFloat Double | VBoolean Bool | VStruct [(Ident, Value)] | Undefined | ErrorOccurred | VIdent Ident
 
 instance Show Value where
     show (VInt i)       = show i
@@ -38,29 +38,32 @@ addVariable :: Env -> Ident -> Value -> Err Env
 addVariable ((scope, f, n):rest) id value = 
     case M.lookup id scope of
       Nothing -> return ((M.insert id value scope, f, n):rest)
-      Just _  -> fail ("Variable " ++ printTree id ++ " already declared.")
+      Just _  -> Bad ("Variable " ++ printTree id ++ " already declared.")
 
--- add function to environment, 
--- if namespace is not defined, add it to global funcitons
--- otherwise add to named namespace
-addFunction :: Env -> Ident -> (Maybe Ident) -> Function_def -> Err Env
-addFunction ((v, scope, n):rest) id Nothing value = 
+addFunction :: Env -> Ident -> Function_def -> Err Env
+addFunction ((v, scope, n):rest) id value = 
     case M.lookup id scope of
       Nothing -> return ((v, M.insert id value scope, n):rest)
-      Just _  -> fail ("Function " ++ printTree id ++ " already declared.")
-addFunction ((v, f, scope):rest) id (Just ns) value = 
-    case M.lookup ns scope of
-      Nothing -> fail ("Namespace " ++ printTree ns ++ " does not exist.")
-      Just (NSDeclarator _ f) -> case lookup (NewFunction _ (FunctionName (VarName id) _) _) f of
-                Nothing -> return ((v, f, M.update ns (NSDeclarator ns (value:f) scope):rest))
-                Just _ -> fail ("Namespace " ++ printTree ns ++ " already has function " ++ id ".")
+      Just _  -> Bad ("Function " ++ printTree id ++ " already declared.")
       
 addNamespace :: Env -> Ident -> Namespace -> Err Env
 addNamespace ((v, f, scope):rest) id value = 
     case M.lookup id scope of
       Nothing -> return ((v, f, M.insert id value scope):rest)
-      Just _  -> fail ("Namespace " ++ printTree id ++ " already declared.")
+      Just _  -> Bad ("Namespace " ++ printTree id ++ " already declared.")
+
       
+setVariable :: Env -> Ident -> Value -> Err Env
+setVariable [] id _ = fail $ "Unknown variable " ++ printTree id ++ "."
+setVariable ((scope, f, n):rest) id val 
+        | M.member id scope = return (((M.update (\v -> Just val) id scope), f, n):rest)  
+        | otherwise = do
+                globalEnv <- return $ setVariable rest id val
+                case globalEnv of
+                        Bad err -> Bad err
+                        Ok env -> return $ (scope, f, n):env
+
+  
 lookupVariable :: Env -> Ident -> Err Value
 lookupVariable [] id = fail ("Variable " ++ printTree id ++ " does not exist!")
 lookupVariable ((scope, f, n):rest) id =
--- interpreter/src/Interpreter.hs
+++ interpreter/src/Interpreter.hs
@@ -20,8 +20,90 @@ interpretProgram :: Program -> State Env Value
 interpretProgram prog = return $ Undefined
 
 interpretStm :: Stm -> State Env Value
-interpretStm stm = return Undefined
+interpretStm stm = return (VInt 2)
 
 interpretExp :: Exp -> State Env Value
-interpretExp exp = return Undefined
+interpretExp exp = case exp of
+        Ecomma e1 e2 -> expComma e1 e2
+        Eassign e1 e2 -> expAssign e1 e2
+        Econdition e1 e2 e3 -> expCondition e1 e2 e3
+        Elor e1 e2 -> expLor e1 e2
+        
+        Econst c -> expConst c
+        otherwise -> return $ Undefined
 
+expComma :: Exp -> Exp -> State Env Value
+expComma e1 e2 = do
+        val1 <- interpretExp e1
+        val2 <- interpretExp e2        
+        return val2
+
+expAssign :: Exp -> Exp -> State Env Value
+expAssign e1 e2 = do
+        ident <- interpretExp e1
+        value <- case ident of
+                VIdent i -> do 
+                        value <- interpretExp e2
+                        env <- get
+                        env' <- do
+                                case (setVariable env i value) of
+                                        Bad err -> error err --TODO: error messages
+                                        Ok  env'' -> return env''
+                        put env'
+                        return value
+                otherwise -> error $ (show ident) ++ " should be an ident" --TODO: error messages
+        return value
+        
+expCondition :: Exp -> Exp -> Exp -> State Env Value
+expCondition e1 e2 e3 = do
+        value <- interpretExp e1
+        value2 <- case value of
+                VInt i -> do
+                        if i /= 0
+                                then interpretExp e2
+                                else interpretExp e3
+                VFloat d -> do
+                        if d /= 0.0
+                                then interpretExp e2
+                                else interpretExp e3
+                VBoolean b -> do
+                        if b
+                                then interpretExp e2
+                                else interpretExp e3
+                otherwise -> error $ (show value) ++ " should be an int, float or bool" --TODO: error messages
+        return value2
+        
+expBool :: Exp -> State Env Value
+expBool e = do
+        value <- interpretExp e
+        bol <- return $ case value of
+                VInt i -> i /= 0
+                VFloat d -> d /= 0.0
+                VBoolean b -> b
+                otherwise -> error $ (show value) ++ " should be an int, float or bool" --TODO: error messages
+        return (VBoolean bol)
+         
+
+expConst :: Constant -> State Env Value
+expConst c = do
+        case c of
+                Efloat c -> return (VFloat c)
+                Ecfloat c -> return (VFloat 0.0) --TODO: check, why it is working without this line
+                Ebool b -> do
+                        boo <- do 
+                                if (b == (Boolean "true"))
+                                        then return True
+                                        else return False
+                        return (VBoolean boo)
+                Eint i -> return (VInt i)
+
+expLor :: Exp -> Exp -> State Env Value                    
+expLor e1 e2 = do
+        bb1 <- expBool e1
+        case bb1 of
+                VBoolean b1 -> do
+                        bb2 <- expBool e2  
+                        case bb2 of
+                                VBoolean b2 -> return $ VBoolean (b1 || b2)
+                                otherwise -> error "Error inside interpreter in function expBool and expLor"
+                otherwise -> error "Error inside interpreter in function expBool and expLor"
